571f998b319787ad5eb1cafb000fada9488b40d6
Bruno Damacena <damacena.bruno@gmail.com>
2022-06-04 10:56:51 -0300

    connect to database on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOSTNAME,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: false,
+      entities: ['src/**/*.entity.ts'],
+      migrations: ['src/database/migrations/*.ts'],
+      logging: ['error'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
